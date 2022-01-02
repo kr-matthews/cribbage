4ce61569ace0b51845d7d2fcf58830888c55f2ae
Kevin Matthews <kvnrmatthews@gmail.com>
2022-01-02 10:11:53 -0800

    Expand useNetwork.
    Include pubnub.
    Rework handling incoming messages (still not good though).
    Error throwing/catching not implemented yet.

--- src/Hooks/useNetwork.js
+++ src/Hooks/useNetwork.js
@@ -1,66 +1,125 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 
 import { useLocalStorage } from "./useLocalStorage.js";
 
+import PubNub from "pubnub";
 // pubnubKeys.js is listed in .gitignore, contains private keys
-// import { subscribeKey, publishKey } from "../pubnubKeys.js";
+import { subscribeKey, publishKey } from "../pubnubKeys.js";
 
 //// Helpers
 
 // should make sure it is unique, but just assume it is
 function createUuid() {
-  // TODO: define getUuid
-  return "1234567890";
+  const possibleChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  const charCount = possibleChars.length;
+  let uuid = "";
+  for (let i = 0; i < 64; i++) {
+    uuid += possibleChars[Math.floor(Math.random() * charCount)];
+  }
+  return uuid;
 }
 
 export function useNetwork({ capacityPerCode = 2 }) {
   //// States & Constants
 
-  // universally unique ID
-  const [uuid, setUuid] = useLocalStorage("uuid", createUuid());
+  // universally unique ID (don't need setter -- won't ever change)
+  const [uuid] = useLocalStorage("uuid", createUuid());
   // code currently in use (can only use one at a time)
   const [code, setCode] = useState(null);
   // function to handle incoming messages
   const [messageHandler, setMessageHandler] = useState(null);
 
+  //// PubNub
+
+  // useMemo to avoid recalculating every reredner due to uuid dependency
+  // (uuid never changes once set)
+  const pubnub = useMemo(() => new PubNub({ publishKey, subscribeKey, uuid }), [
+    uuid,
+  ]);
+
   //// Effects
 
+  // listen for incoming messages
   useEffect(() => {
     if (messageHandler) {
-      // TODO: NETWORK: setup subscription
+      console.info("Adding handler for incoming messages");
+      const listener = { message: messageHandler };
+      pubnub.addListener(listener);
+
+      return function cleanupListener() {
+        console.info("Removing handler for incoming messages");
+        pubnub.removeListener(listener);
+      };
     }
-  });
+  }, [messageHandler, pubnub]);
 
   //// Helpers
 
-  // ...
+  // randomly generate codes until an unused one is found
+  function getUnusedCode() {
+    // TODO: define getUnusedCode
+    return "XXXX";
+  }
+
+  // subscribe to incoming messages
+  function subscribeTo(code) {
+    console.info("Subscribing to $code");
+    pubnub.subscribe({ channels: [code], withPresence: true });
+  }
+
+  function unsubscribeFrom(code) {
+    console.info("Unsubscribing from $code");
+    pubnub.unsubscribe({ channels: [code] });
+  }
 
   //// Return functions
 
   // generate an unused code and setup incoming/outoing messages
   function create() {
-    // TODO: NETWORK: define create
-    // will throw errors if can't create (network error, no unused codes, etc?)
-    setCode("XXXX");
+    try {
+      const newCode = getUnusedCode();
+      subscribeTo(newCode);
+      setCode(newCode);
+    } catch (e) {
+      // TODO: NETWORK: handle errors in create
+      // will throw errors if can't create (network error, no unused codes, etc?)
+    }
   }
 
   // if allowed, setup incoming/outgoing messages
   function join(codeInput) {
-    // TODO: NETWORK: define join
-    // will throw errors if can't join (network error, room empty, room full, etc?)
+    try {
+      subscribeTo(codeInput);
+      setCode(codeInput);
+    } catch (e) {
+      // TODO: NETWORK: handle errors in join
+      // will throw errors if can't join (network error, room empty, room full, etc?)
+    }
     setCode(codeInput);
   }
 
   // clean exit from room, closing incoming/outgoing messages; notify others
   function leave() {
-    // TODO: NETWORK: define leave
-    // shouldn't throw any errors
-    setCode(null);
+    try {
+      unsubscribeFrom(code);
+    } catch (e) {
+      // TODO: NETWORK: handle errors in leave
+      // leave won't throw any errors
+    } finally {
+      setCode(null);
+    }
   }
 
   // send the message to the
   function sendMessage(message) {
-    // TODO: NETWORK: define sendMessage
+    if (!code) {
+      // TODO: NETWORK: throw error
+    }
+    try {
+      pubnub.publish({ message: { ...message, uuid }, channel: code });
+    } catch (e) {
+      // TODO: NETWORK: handle errors in sendMessage
+    }
   }
 
   //// Return
@@ -71,6 +130,6 @@ export function useNetwork({ capacityPerCode = 2 }) {
     join,
     leave,
     sendMessage,
-    setMessageHandler,
+    setMessageHandler, // maybe pass in via object which never changes?
   };
 }
