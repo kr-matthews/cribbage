8f21cd76e9c9b04d69c6385ac40a56cb6939424d
Kevin Matthews <kvnrmatthews@gmail.com>
2022-08-30 13:13:58 -0700

    Fix match log to use 'you' instead of name

--- src/hooks/useMatchLogs.js
+++ src/hooks/useMatchLogs.js
@@ -69,7 +69,7 @@ export function useMatchLogs(
       let message = {
         type: "auto",
         colour: players[player].colour,
-        text: `${players[player].name} `, // !! is no longer "You"
+        text: `${players[player].isUser ? "You" : players[player].name} `,
         timestamp: Date.now(),
       };
       const s = player === userPosition ? "" : "s";
